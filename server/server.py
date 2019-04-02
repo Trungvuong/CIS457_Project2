c681971d56e63599887d7e931e7660606d5aa179
Trungvuong <phamtru@DESKTOP-B0P4DA8.localdomain>
2019-04-02 00:19:52 -0400

    Updated server.py

--- server/server.py
+++ server/server.py
@@ -1,17 +1,37 @@
-import os
+'''
+This is an FTP Server that connects users to the server.
 
+@author Trung-Vuong Pham, Ryan Eisenbarth, and Kevin Holekboer
+@version 1.0
+@date April 3, 2019
+
+'''
+
+import os
+import socket
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
-from pyftpdlib.servers import FTPServer
+from pyftpdlib.servers import ThreadedFTPServer
 
+def main():
+    # Creates authorization for users
+    authorizer = DummyAuthorizer()
+    authorizer.add_anonymous('.', perm='elradfmwM')
 
-authorizer = DummyAuthorizer()
-authorizer.add_user("user", "pass", ".", perm="elradfmw")
-authorizer.add_anonymous(os.getcwd())
+    #Handle requests sent by the server
+    handler = FTPHandler
+    handler.authorizer = authorizer
+    handler.banner= "Connected"
+    
+    #Opens pipe and sockets
+    pipe = os.popen("ip -4 route show default").read().split()
+    sckt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sckt.connect((pipe[3], 0))
+    address = sckt.getsockname()[0]
 
-handler = FTPHandler
-handler.authorizer = authorizer
-handler.banner= "Connected"
+    #Creates server on port 3000 on localhost
+    server = ThreadedFTPServer((address, 3000), handler)
+    server.serve_forever()
 
-server = FTPServer(("127.0.0.1", 3000), handler)
-server.serve_forever()
+if __name__ == "__main__":
+    main()
