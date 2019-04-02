874c07f9a8aeed90dddb29d4a692f80c31b419b3
Trungvuong <phamtru@DESKTOP-B0P4DA8.localdomain>
2019-04-02 00:35:29 -0400

    Fixed some syntax errors

--- server/server.py
+++ server/server.py
@@ -18,18 +18,18 @@ def main():
     authorizer = DummyAuthorizer()
     authorizer.add_anonymous('.', perm='elradfmwM')
 
-    #Handle requests sent by the server
+    # Handle requests sent by the server
     handler = FTPHandler
     handler.authorizer = authorizer
     handler.banner= "Connected"
     
-    #Opens pipe and sockets
+    # Opens pipe and sockets
     pipe = os.popen("ip -4 route show default").read().split()
     sckt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sckt.connect((pipe[3], 0))
     address = sckt.getsockname()[0]
 
-    #Creates server on port 3000 on localhost
+    # Creates server on port 3000 on localhost
     server = ThreadedFTPServer((address, 3000), handler)
     server.serve_forever()
 
